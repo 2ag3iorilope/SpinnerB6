eba055d63e1d74287392f3a9747039873decf21e
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-12-10 09:15:00 +0100

    Last

--- app/src/main/java/com/ikaslea/spinnerb6/MainActivity.java
+++ app/src/main/java/com/ikaslea/spinnerb6/MainActivity.java
@@ -3,6 +3,7 @@ package com.ikaslea.spinnerb6;
 import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -14,14 +15,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spinnerHizkuntzak;
+    ListView ListviewHizkuntzak;
     TextView textViewHizkuntzaTestua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinnerHizkuntzak = findViewById(R.id.spinner);
+        ListviewHizkuntzak = findViewById(R.id.ListView1);
         textViewHizkuntzaTestua = findViewById(R.id.textView);
 
         String[] Hizkuntzak = {
@@ -39,19 +40,15 @@ public class MainActivity extends AppCompatActivity {
                 getString(R.string.frase_aleman),
                 getString(R.string.frase_noruego)
         };
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Hizkuntzak);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerHizkuntzak.setAdapter(adapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Hizkuntzak);
+        ListviewHizkuntzak.setAdapter(adapter);
 
-        spinnerHizkuntzak.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        ListviewHizkuntzak.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, android.view.View view, int position, long id) {
+            public void onItemClick(AdapterView<?> parent, android.view.View view, int position, long id) {
 
                 textViewHizkuntzaTestua.setText(Esaldiak[position]);
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
 
